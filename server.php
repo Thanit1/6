ee74e2dbd0d5fa42fc073ff96106b9d8f8e81c81
thanin1 <144163583+thanin1@users.noreply.github.com>
2023-09-30 02:03:29 +0700

    Delete server.php

--- server.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $db_host = "localhost";
-    $db_username = "root";
-    $db_password = "0656076916";
-    $db_name = "dbmini-project";
-    $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
-?>
